2aab48366d9a2d2d14e5a7a9da9773652e4fe5c9
Willian <wsapcf@gmail.com>
2019-08-01 14:15:56 -0500

    Validator tweaked

--- validators/validation.js
+++ validators/validation.js
@@ -4,7 +4,7 @@ const Joi = require("@hapi/joi");
 const registerValidation = data => {
   const schema = {
     name: Joi.string()
-      .min(6)
+      .min(2)
       .required(),
     email: Joi.string()
       .min(6)
