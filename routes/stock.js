f79040f08d53cce7b5fee55f00ac2fa71a393a76
Willian S <wsapcf@gmail.com>
2019-10-23 16:46:37 -0300

    Quantity assign before return

--- routes/stock.js
+++ routes/stock.js
@@ -59,7 +59,8 @@ router.get("/stock/getStock", async (req, res) => {
 
   try {
     if (user) {
-      res.status(200).json({quantity: user.initialStock});
+      const quantity = user.initialStock;
+      res.status(200).json({ quantity: quantity });
     } else {
       res.status(400).json({ msg: "Erro no retorno" });
     }
