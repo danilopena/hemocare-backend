1846ec3b126b1c23fcdb214b0563b118a283c8c9
Willian S <wsapcf@gmail.com>
2019-11-29 18:07:53 -0300

    Fix 400 status message on catch

--- validation.js
+++ validation.js
@@ -3,16 +3,36 @@ const Joi = require("@hapi/joi");
 const registerValidation = data => {
   const schema = {
     name: Joi.string()
-      .min(2, 'O nome deve conter no mínimo 2 caracteres.')
-      .required('O nome deve ser informado'),
+      .min(2)
+      .required('O nome deve ser informado').error(() => {
+          return {
+            message: 'Nome deve conter no mínimo duas letras.',
+          };
+        }),
     email: Joi.string()
       .min(6)
-      .email('Por gentileza, insira um email válido'),
+      .email().error(() => {
+          return {
+            message: 'Insira um email válido, por gentileza.',
+          };
+        }),
     password: Joi.string()
-      .min(6, 'A senha deve ter no mínimo 6 caracteres')
-      .required('É obrigatório fornecer uma senha'),
-    pathology: Joi.string().required(),
-    agreeToTerms: Joi.boolean().required()
+      .min(6)
+      .required().error(() => {
+          return {
+            message: 'Sua senha deve conter no mínimo 6 caracteres',
+          };
+        }),
+    pathology: Joi.string().required().error(() => {
+      return {
+        message: 'Por gentileza, informe seu tipo de coagulopatia',
+      };
+    }),
+    agreeToTerms: Joi.boolean().required().error(() => {
+      return {
+        message: 'Para prosseguir, os termos de uso devem ser aceitos. '
+      };
+    }),
   };
   return Joi.validate(data, schema);
 };
